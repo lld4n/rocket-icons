ce1fdac35f4564ee62361af34d5aed8476ef94b8
lldan <safinildan@gmail.com>
2024-08-31 23:09:00 +0300

    0.4.3: add rocket map

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import { common, ext, files, folders } from "./src";
 
 export * from "./src";
 
-export const rocket = [...folders, ...ext, ...common, ...files];
+export const rocket = {
+  ["folders"]: folders,
+  ["ext"]: ext,
+  ["common"]: common,
+  ["files"]: files,
+};
