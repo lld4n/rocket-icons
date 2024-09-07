6c20ae9bbb295cbc730df042a0905c11ad092c9a
lldan <safinildan@gmail.com>
2024-09-07 19:43:37 +0300

    0.5.3: mini-fix

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import { common, ext, files, folders } from "./src";
 export * from "./src";
 
 export const rocket = {
-  ["folders"]: folders,
   ["ext"]: ext,
   ["common"]: common,
   ["files"]: files,
