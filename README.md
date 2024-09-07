84db671df800d2c62d79eec780a3bfb7fd6457db
lldan <safinildan@gmail.com>
2024-09-07 21:46:18 +0300

    0.5.5: update readme

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # Rocket Icons
 
-- [DUO](docs/DUO.md)
-- [FILLED](docs/FILLED.md)
-- [OUTLINE](docs/OUTLINE.md)
+<!--BADGE--->
+[![](https://img.shields.io/badge/Figma-FF3E00)](https://www.figma.com/design/WF5eDX3KsJMzmVXTjWxv72/Rocket-Icons?node-id=0-1&t=ujglndRAjCvTZIG9-1)
+[![](https://img.shields.io/badge/JetBrains%20Plugin-0366D6)](https://github.com/lld4n/rocket-icons-jetbrains)
+[![](https://img.shields.io/badge/Author-000000)](https://github.com/lld4n)
+[![](https://img.shields.io/badge/LICENSE%20MIT-000000)](https://github.com/lld4n/rocket-icons/tree/master/LICENSE)
+[![](https://img.shields.io/badge/0.5.5-000000)](https://github.com/lld4n/rocket-icons/releases)
+<!--BADGE_END--->
+
+Icon Pack for plugins:
+
+- [Rocket Icons JetBrains](https://github.com/lld4n/rocket-icons-jetbrains)
+- Rocket Icons VSCode (TBD)
+
+The icons were designed with an eye towards minimalism. Two colors were used in the icons in most cases. Two options for displaying folder icons: rectangular and in folder format. The icons are available in 4 variants:
+
+- [DUO](https://github.com/lld4n/rocket-icons/tree/master/docs/DUO)
+- [OUTLINE](https://github.com/lld4n/rocket-icons/tree/master/docs/OUTLINE)
+- [FILLED](https://github.com/lld4n/rocket-icons/tree/master/docs/FILLED)
+- [REVERSE](https://github.com/lld4n/rocket-icons/tree/master/docs/REVERSE)
+
+## Preview
+
+![DUO](./docs/icons/duo.svg)
+![OUTLINE](./docs/icons/outline.svg)
+![FILLED](./docs/icons/filled.svg)
+![REVERSE](./docs/icons/reverse.svg)
+
+
+
--- deploy.ts
+++ deploy.ts
@@ -10,31 +10,15 @@ import {
   IconVariant,
 } from "./src";
 
-const PACKAGE_JSON = JSON.parse(fs.readFileSync("./package.json").toString());
+// types
 
-const args = process.argv;
-const version = PACKAGE_JSON.version;
-
-const oldVersion = fs.readFileSync("./version").toString();
-
-if (oldVersion === version) {
-  throw new Error("Неправильные версии");
+interface Badge {
+  text: string;
+  url: string;
+  color: string;
 }
 
-fs.writeFileSync("./version", version);
-
-let message = "";
-let no_deploy = false;
-
-for (let i = 0; i < args.length; i++) {
-  if (args[i] === "-m") {
-    if (args[i + 1]) {
-      message = ": " + args[i + 1];
-    }
-  } else if (args[i] === "-no") {
-    no_deploy = true;
-  }
-}
+// constants
 
 const map: { [type in IconType]: Icon[] } = {
   [IconType.FOLDERS]: folders,
@@ -43,6 +27,69 @@ const map: { [type in IconType]: Icon[] } = {
   [IconType.FILES]: files,
   [IconType.EXT]: ext,
 };
+const BADGE_START = "<!--BADGE--->";
+const BADGE_END = "<!--BADGE_END--->";
+const PACKAGE_JSON = JSON.parse(fs.readFileSync("./package.json").toString());
+const args = process.argv;
+const version = PACKAGE_JSON.version;
+const oldVersion = fs.readFileSync("./version").toString();
+const badges: Badge[] = [
+  {
+    text: "Figma",
+    url: "https://www.figma.com/design/WF5eDX3KsJMzmVXTjWxv72/Rocket-Icons?node-id=0-1&t=ujglndRAjCvTZIG9-1",
+    color: "FF3E00",
+  },
+  {
+    text: "JetBrains Plugin",
+    url: "https://github.com/lld4n/rocket-icons-jetbrains",
+    color: "0366D6",
+  },
+  {
+    text: "Author",
+    url: "https://github.com/lld4n",
+    color: "000000",
+  },
+  {
+    text: "LICENSE MIT",
+    url: "https://github.com/lld4n/rocket-icons/tree/master/LICENSE",
+    color: "000000",
+  },
+  {
+    text: version,
+    url: "https://github.com/lld4n/rocket-icons/releases",
+    color: "000000",
+  },
+];
+
+// arguments from cli
+
+let message = "";
+let no_deploy = false;
+let no_version = false;
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === "-m") {
+    if (args[i + 1]) {
+      message = ": " + args[i + 1];
+    }
+  } else if (args[i] === "-no-deploy") {
+    no_deploy = true;
+  } else if (args[i] === "-no-version") {
+    no_version = true;
+  }
+}
+
+// version
+
+if (!no_version) {
+  if (oldVersion === version) {
+    throw new Error("Неправильные версии");
+  }
+
+  fs.writeFileSync("./version", version);
+}
+
+// docs
 
 for (const variant in IconVariant) {
   const VARIANT = variant.toUpperCase();
@@ -66,6 +113,32 @@ for (const variant in IconVariant) {
     fs.writeFileSync(`./docs/${VARIANT}/${TYPE}.md`, md);
   }
 }
+
+// readme
+
+const readme = fs.readFileSync("./README.md").toString();
+const readmeBuffer = readme.split("\n");
+
+let md = "";
+let work = false;
+for (const line of readmeBuffer) {
+  if (line === BADGE_START) {
+    work = true;
+    md += BADGE_START + "\n";
+    for (const b of badges) {
+      md += `[![](https://img.shields.io/badge/${b.text.replace(" ", "%20")}-${b.color})](${b.url})\n`;
+    }
+  }
+  if (line === BADGE_END) work = false;
+
+  if (!work) {
+    md += line + "\n";
+  }
+}
+
+fs.writeFileSync("./README.md", md);
+
+// push
 
 if (!no_deploy) {
   const commitMessage = version + message;
